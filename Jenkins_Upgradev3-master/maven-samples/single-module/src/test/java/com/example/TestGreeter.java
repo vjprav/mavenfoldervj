e42e3f68d44e84f026c74deb8c547f8d23a2eff5
vjprav <vj.prav18@gmail.com>
2023-05-28 13:15:55 -0400

    removing extra test case

--- Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,11 +27,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
   
-  @Test
-  public void intentionally_Failed() {
-   assertTrue(true);
-  }
-  
+ 
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
